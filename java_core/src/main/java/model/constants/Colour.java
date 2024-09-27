f443854763f9d601ad3ac70c7ad941e03f6d98ad
Pantazi Dmitrii <mage9111@gmail.com>
2024-09-27 21:36:02 +0300

    delete project

--- java_core/src/main/java/model/constants/Colour.java
+++
@@ -1,6 +0,0 @@
-package model.constants;
-
-public class Colour {
-    public final static String COLOUR_RED = "red";
-    public final static String COLOUR_GREEN = "green";
-}
--- java_core/src/main/java/model/constants/Discount.java
+++
@@ -1,6 +0,0 @@
-package model.constants;
-
-public class Discount {
-    public final static double SIXTY_PERCENT_DISCOUNT = 0.6;
-    public final static double ZERO_PERCENT_DISCOUNT = 0.0;
-}
